e0dbbfa6a04896c01c6ce6cf9bd9f5eb37297d14
MR R A Y H A N <mrrayhan246@gmail.com>
2025-02-04 20:40:31 +0600

    Update admin.py

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed', 'created_at', 'due_date', 'priority')
+    list_filter = ('completed', 'priority', 'due_date')
+    search_fields = ('title', 'description')
+
+admin.site.register(Task, TaskAdmin)
