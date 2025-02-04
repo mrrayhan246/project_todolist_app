da3a992d01d50e2881307468abd5a410a41ebf46
MR R A Y H A N <mrrayhan246@gmail.com>
2025-02-04 20:37:56 +0600

    Update

--- apps/tasks/forms.py
+++ apps/tasks/forms.py
@@ -4,4 +4,13 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title']
+        fields = ['title', 'description', 'due_date', 'priority', 'completed']
+
+    due_date = forms.DateTimeField(
+        widget=forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'})
+    )
+
+    priority = forms.ChoiceField(
+        choices=Task.PRIORITY_CHOICES,
+        widget=forms.RadioSelect
+    )
